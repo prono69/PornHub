3b28552ff21bcbb9c3cb5cecccc4afb51aa0ea71
prono69 <faltu9851@gmail.com>
2020-08-10 17:44:37 +0530

    hmm

--- stdplugins/heroku.py
+++ stdplugins/heroku.py
@@ -15,7 +15,10 @@ HEROKU_APP_NAME = Config.HEROKU_APP_NAME
 HEROKU_API_KEY = Config.HEROKU_API_KEY
 
 # Here lies the Magic
-
+useragent = ('Mozilla/5.0 (Linux; Android 10; SM-G975F) '
+                 'AppleWebKit/537.36 (KHTML, like Gecko) '
+                 'Chrome/80.0.3987.149 Mobile Safari/537.36'
+                 )
 
 @borg.on(admin_cmd(pattern=r"(set|get|del) var ?(.*)", allow_sudo=True))
 async def variable(var):
