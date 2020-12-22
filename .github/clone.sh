b22d33285d5f15119972f1be379dd8a1c2979942
prono69 <faltu9851@gmail.com>
2020-12-22 11:15:25 +0530

    hm

--- .github/clone.sh
+++ .github/clone.sh
@@ -30,23 +30,15 @@ if [ -d "$FILE" ] ; then
     echo "$FILE directory exists already."
 else
     rm -rf userbot
-    rm -rf dbplugins
-    rm -rf sql_helpers
-    rm -rf stdplugins
-    rm -rf uniborg
     rm -rf .github
     rm -rf requirements.txt
     git clone https://github.com/prono69/PepeBot pepe
     mv pepe/userbot .
-    mv pepe/dbplugins .
-    mv pepe/sql_helpers .
-    mv pepe/stdplugins .
-    mv pepe/uniborg .
     mv pepe/.github . 
     mv pepe/.git .
     mv pepe/requirements.txt .
     rm -rf pepe
-    python ./.github/update.py
+    python3 ./.github/update.py
 fi
 
 FILE=/app/bin/
