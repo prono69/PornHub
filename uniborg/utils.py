9c51308ba6bd1c6aae560af5ca2fbaeac3007348
Shrimadhav U K <scaleway@shrimadhavuk.me>
2021-12-28 21:18:07 +0530

    add function to get chat id and message id from message link, without making API request

--- uniborg/utils.py
+++ uniborg/utils.py
@@ -7,7 +7,7 @@ import re
 import math
 import os
 import time
-from typing import List
+from typing import List, Tuple, Union
 from telethon import events
 from telethon.utils import add_surrogate
 from telethon.tl.custom import Message
@@ -407,3 +407,20 @@ def get_message_link(message: Message, fp_: bool) -> str:
     return (
         f"https://t.me/c/{chat_id}/{message_id}"
     )
+
+
+def get_c_m_message(message_link: str) -> Tuple[Union[str, int], int]:
+    p_m_link = message_link.split("/")
+    chat_id, message_id = None, None
+    if len(p_m_link) == 6:
+        # private link
+        if p_m_link[3] == "c":
+            # for supergroups / channels
+            chat_id, message_id = int("-100" + p_m_link[4]), int(p_m_link[5])
+        elif p_m_link[4] == "UniBorg":
+            # for basic groups / private chats
+            chat_id, message_id = int(p_m_link[4]), int(p_m_link[5])
+    elif len(p_m_link) == 5:
+        # public link
+        chat_id, message_id = str("@" + p_m_link[3]), int(p_m_link[4])
+    return chat_id, message_id
